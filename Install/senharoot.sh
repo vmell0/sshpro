953e39b658ed7d41b2fd580a6ca0a7835bb9e810
vmell0 <152212726+vmell0@users.noreply.github.com>
2024-07-08 17:13:05 -0300

    1.0.0.4

--- Install/senharoot.sh
+++ Install/senharoot.sh
@@ -1,20 +1,55 @@
 #!/bin/bash
+# Pequeno script para permissao de autenticacao root
 clear
-
-executa_instalador() {
-    wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/vmell0/sshpro/main/Install/sshd_config
+[[ "$(whoami)" != "root" ]] && {
+	clear
+	echo -e "\033[1;31mEXECULTE COMO USUARIO ROOT, \033[1;32m(\033[1;33msudo -i\033[1;32m)\033[0m"
+	exit
 }
 
-# - Execução
-sudo -i
-executa_instalador
-service ssh restart
-clear
-echo -ne "\033[1;32mDigite sua nova senha root\033[1;37m: "; read senha
+iptables -F
+echo 'nameserver 1.1.1.1' > /etc/resolv.conf
+echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+sudo apt update -y
+
+[[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "without-password" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/without-password/yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "#PermitRootLogin" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "PasswordAuthentication" /etc/ssh/sshd_config) = '0' ]] && {
+	echo 'PasswordAuthentication yes' > /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "#PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+} > /dev/null
+
+
+sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config.d/60-cloudimg-settings.conf  > /dev/null
+
+
+#sed -i "s/ChallengeResponseAuthentication no/#ChallengeResponseAuthentication no/g" /etc/ssh/sshd_config
+
+service ssh restart > /dev/null
+
+iptables -F
+iptables -A INPUT -p tcp --dport 81 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8799 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+iptables -A INPUT -p tcp --dport 1194 -j ACCEPT
+clear && echo -ne "\033[1;32mDEFINA A SENHA ROOT\033[1;37m: "; read senha
 [[ -z "$senha" ]] && {
-echo -e "    Calma barboleta, vê se não erra de novo\033"
+echo -e "\n\033[1;31mSENHA INVALIDA !\033[0m"
 exit 0
 }
 echo "root:$senha" | chpasswd
-echo -e "    Senha alterada com Sucesso"
-exit
+echo -e "\n\033[1;31m[ \033[1;33mOK ! \033[1;31m]\033[1;37m - \033[1;32mSENHA DEFINIDA ! \033[0m"
